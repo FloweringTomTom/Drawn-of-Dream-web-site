059a508535d85602888b96c7f3c50fa3a02dcbfe
ethus3h <kolubat@gmail.com>
2017-01-19 15:03:38 -0500

    Update Ember tree

--- futuramerlin-web-toolkit-output/ancillary/wreathe/7r2/download.php
+++ futuramerlin-web-toolkit-output/ancillary/wreathe/7r2/download.php
@@ -1,1 +1,1 @@
-<?php header('Location: https://archive.org/download/Wreathe7r2'); exit(); ?>
+<?php header('Location: https://archive.org/download/Wreathe7r2/w7-snapshot-as-of-2Sept2014.dd'); exit(); ?>
