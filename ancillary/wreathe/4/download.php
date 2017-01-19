7e14451dfcf6032dd0630f2f2c7ff30ca53f1a32
ethus3h <kolubat@gmail.com>
2017-01-19 13:57:59 -0500

    Update Ember tree

--- ancillary/wreathe/4/download.php
+++ ancillary/wreathe/4/download.php
@@ -1,1 +1,1 @@
-<?php header('Location: https://archive.org/download/Wreathe4'); exit(); ?>
+<?php header('Location: https://archive.org/download/Wreathe4/wreathe4.iso'); exit(); ?>
--- ancillary/wreathe/6/download.php
+++ ancillary/wreathe/6/download.php
@@ -1,1 +1,1 @@
-<?php header('Location: https://archive.org/download/Wreathe6'); exit(); ?>
+<?php header('Location: https://archive.org/download/Wreathe6/wreathe6.iso'); exit(); ?>
