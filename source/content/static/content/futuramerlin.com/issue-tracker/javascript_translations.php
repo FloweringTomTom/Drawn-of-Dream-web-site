bc53db329997432f42a9186c46073f12fbaaf5ff
ethus3h <kolubat@gmail.com>
2017-11-28 12:26:55 -0500

    Update Ember tree

---
+++ source/content/static/content/futuramerlin.com/issue-tracker/javascript_translations.php
@@ -0,0 +1,4 @@
+var translations = new Array();
+translations['time_tracking_stopwatch_start'] = 'Start';
+translations['time_tracking_stopwatch_stop'] = 'Stop';
+translations['loading'] = 'Loading...';
