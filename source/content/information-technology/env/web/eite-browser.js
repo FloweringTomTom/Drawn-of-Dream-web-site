2176743346760b3db9e1ebb22cc34f3a2b008573
ethus3h <kolubat@gmail.com>
2018-03-26 09:15:25 -0400

    Update Ember tree

---
+++ source/content/information-technology/env/web/eite-browser.js
@@ -0,0 +1,170 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+// This file defines functions with implementations that are specific to Web browser implementation of EITE in JavaScript.
+// This is a library file, and should only initialize functions/variables, so that it can be loaded and run in parallel with other library files, and work regardless of the order they are loaded.
+
+// Platform-specific overrides of routines available portably
+
+// Override error reporting method to show alert
+function voidEiteError(strMessage) {
+    console.trace();
+    eiteLog('EITE reported error!: '+implNormalizeMessage(strMessage));
+    alert('EITE reported error!: '+implNormalizeMessage(strMessage));
+    throw 'EITE reported error!: '+implNormalizeMessage(strMessage);
+}
+function eiteWarn(strMessage) {
+    console.trace();
+    eiteLog('EITE reported warning: '+implNormalizeMessage(strMessage));
+    alert('EITE reported warning: '+implNormalizeMessage(strMessage));
+}
+
+// Fully platform-specific code
+
+function assertIsString(str) {
+    if (typeof str !== "string") {
+        eiteError("Assertion failed: "+str+" is not a string.")
+    }
+}
+
+function die(strMessage) {
+    throw strMessage;
+}
+
+function implEiteLog(strMessage) {
+    // This function implements logging (which may differ between platforms).
+    console.log(implNormalizeMessage(strMessage));
+};
+
+function implGetEnvironmentBestFormat() {
+    return 'immutableCharacterCells';
+}
+
+function implGetEnvironmentRenderTraits(targetFormat) {
+    if ( targetFormat === undefined ) {
+        eiteError('implGetEnvironmentRenderTraits was called without any targetFormat!');
+    }
+    var traits = {};
+    switch (targetFormat) {
+        case 'integerList':
+        case 'immutableCharacterCells':
+            traits.cellTableWidth = -1; // unlimited
+            traits.cellTableHeight = -1; // unlimited
+            let cs = document.characterSet.toLowerCase();
+            switch(cs) {
+                case 'utf-8':
+                    traits.characterEncoding = 'UTF-8';
+                    break;
+                default:
+                    eiteWarn('Unimplemented character set: '+cs+'. Falling back to ASCII-safe-subset.');
+                    traits.characterEncoding = 'ASCII-safe-subset';
+                    break;
+            }
+            break;
+    }
+    return traits;
+}
+
+function loadCsv(url, lineLoadedCallback, documentLoadedCallback, errorCallback) {
+    Papa.parse(url, {
+        download: true,
+        encoding: 'UTF-8',
+        newline: "\n",
+        delimiter: ',',
+        quoteChar: '"',
+        step: function(results, parser) {
+            lineLoadedCallback(results, parser);
+        },
+        complete: function(results, file) {
+            documentLoadedCallback(results, file);
+        },
+        error: function(results, file) {
+            errorCallback(results, file);
+        }
+    })
+}
+
+function implDoRenderIo(renderBuffer, targetFormat) {
+    switch (targetFormat) {
+        case 'integerList':
+        case 'immutableCharacterCells':
+            let immutableCharCellOutput = document.getElementById('log');
+            for (let i = 0; i < renderBuffer.length; i++) {
+                immutableCharCellOutput.innerHTML += implNormalizeMessage(renderBuffer[i]) + '<br />';
+                immutableCharCellOutput.scrollTop = immutableCharCellOutput.scrollHeight;
+            }
+            break;
+        default:
+            eiteError('Unimplemented render I/O format: '+targetFormat);
+            break;
+    }
+}
+
+function urlLoadForCallback(url, callback) {
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+    oReq.onload = function(oEvent) {
+        callback(new Uint8Array(oReq.response)); // Note: not oReq.responseText
+    };
+    oReq.send(null);
+}
+
+function operateOnDocFromUrl(strFormat, strUrl, callback) {
+    urlLoadForCallback(strUrl, function(bytearrayContent) { callback(dcarrParseDocument(strFormat, bytearrayContent)); })
+}
+
+function implStrFromUnicodeHex(strCharacter) {
+    return String.fromCharCode('0x'+strCharacter);
+}
+
+function runEiteTest(strTestFormat, strTestName) {
+    // TODO: Unfinished implementation
+    strTestUrlPrefix='../tests/'+strTestName+'.'+strTestFormat+'/';
+    strTestInputFormatUrl='../tests/'+strTestName+'.'+strTestFormat+'/in-format';
+    switch (strTestFormat) {
+        case 'ept': // Parser test
+            urlLoadForCallback(strTestInputFormatUrl, function(bytearrayContent) {})
+            break;
+        default:
+            eiteError('Unimplemented test format: '+strTestFormat);
+            break;
+    }
+}
+
+// Set up dcData
+dcData = [];
+function dcDataAppendDataset(dataset) {
+    dcData[dataset] = [];
+}
+function dcDataAppendLine(dataset, line) {
+    dcData[dataset].push(line);
+}
+function loadDatasets(callback) {
+    if (datasets.length > 0) {
+        let dataset = datasets[0];
+        dcDataAppendDataset(dataset);
+        loadCsv(
+            '../data/'+dataset+'.csv',
+            function(results,parser){
+                dcDataAppendLine(dataset, results);
+            },
+            function(){
+                datasets.shift();
+                loadDatasets(callback);
+            },
+            function(){
+                eiteError('Error reported while parsing '+dataset+'!')
+            }
+        );
+    }
+    else {
+        callback();
+    }
+}
+
+datasets=[
+    'DcData',
+    'mappings/from/ascii',
+    'mappings/from/unicode',
+    'mappings/to/html'
+];
+// @license-end
---
+++ source/content/information-technology/env/web/eite.js
@@ -0,0 +1,224 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+// This file contains portable code for all JavaScript implementations of EITE.
+// This is a library file, and should only initialize functions/variables, so that it can be loaded and run in parallel with other library files, and work regardless of the order they are loaded.
+// It also serves as an example implementation that should be written in a way that is easily ported to other platforms.
+// To serve these two goals, it should not use I/O, objects, first-class functions, JavaScript-specific libraries, or null/undefined values.
+// Things that depend on I/O and JavaScript-specific libraries (e.g. logging using JSON.stringify) should be implemented in eite-[platform].js  (for platform-specific code) or eite-nonportable.js for JavaScript-specific code.
+// Those files should use clearly defined APIs that this file's code can call, so that they can be implemented as appropriate in other implementations.
+// dcData object must be available before calling these functions.
+// TODO: Function parameters and return values should be type-checked to ensure their validity. Similarly, the string types that correspond to a set of possible values (format names, encoding names, etc.) should be checked against the set (this could also be reflected in more specific/meaningful identifier prefixes).
+
+function eiteLog(strMessage) {
+    assertIsString(strMessage);
+    implEiteLog(strMessage);
+}
+function eiteWarn(strMessage) {
+    assertIsString(strMessage);
+    eiteLog('EITE reported warning: '+strMessages);
+}
+function eiteError(strMessage) {
+    assertIsString(strMessage);
+    eiteLog('EITE reported error!: '+strMessage);
+    die('EITE reported error!: '+strMessage);
+}
+
+// Tools for Dc text
+{
+    function intDcIdToCsvRow(dc) {
+        return parseInt(dc) + 1;
+    }
+    function strDcDataLookupById(strDataset, dc, intFieldNumber) {
+        return dcData[strDataset][intDcIdToCsvRow(dc)].data[0][intFieldNumber];
+    }
+    function strDcDataLookupByValue(strDataset, filterField, filterValue, desiredField) {
+        let intLength = dcData[strDataset].length;
+        // start at 1 to skip header row
+        for (let intRow = 1; intRow < intLength; intRow++) {
+            if(dcData[strDataset][intRow].data[0][filterField] === filterValue) {
+                return dcData[strDataset][intRow].data[0][desiredField];
+            }
+        }
+    }
+    function strDcGetField(dc, intFieldNumber) {
+        return strDcDataLookupById("DcData", dc, intFieldNumber);
+    }
+    function strDcGetName(dc) {
+        return strDcGetField(dc, 1);
+    }
+    function strDcGetCombiningClass(dc) {
+        return strDcGetField(dc, 2);
+    }
+    function strDcGetBidiClass(dc) {
+        return strDcGetField(dc, 3);
+    }
+    function strDcGetCasing(dc) {
+        return strDcGetField(dc, 4);
+    }
+    function strDcGetType(dc) {
+        return strDcGetField(dc, 5);
+    }
+    function strDcGetScript(dc) {
+        return strDcGetField(dc, 6);
+    }
+    function strDcGetComplexTraits(dc) {
+        return strDcGetField(dc, 7);
+    }
+    function strDcGetDescription(dc) {
+        return strDcGetField(dc, 8);
+    }
+
+    function boolDcIsNewline(dc) {
+        if(strDcGetBidiClass(dc) === 'B') {
+            return true;
+        }
+        return false;
+    }
+
+    function boolDcIsPrintable(dc) {
+        strType=strDcGetType(dc);
+        strGeneralType=strType[0];
+        switch(strType) {
+            case 'Zl':
+            case 'Zp':
+                return false;
+                break;
+            default:
+                break;
+        }
+        switch(strGeneralType) {
+            case '!':
+            case 'C':
+                return false;
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+}
+
+// Tools for ASCII text
+{
+    // Checks whether N is within the range A and B, including endpoints
+    function boolIsBetween(intN, intA, intB) {
+        return (intN - intA) * (intN - intB) <= 0;
+    }
+    function boolIsDigit(intN) {
+        return boolIsBetween(intN, 48, 57);
+    }
+    function boolIsPrintable(intN) {
+        return boolIsBetween(intN, 32, 126);
+    }
+    function boolIsSpace(intN) {
+        return intN == 32;
+    }
+    function boolIsNewline(intN) {
+        return (intN == 10) || (intN == 13);
+    }
+    /*
+    0  NUL    16 DLE    32 SP   48 0    64 @    80 P    96  `    112 p
+    1  SOH    17 DC1    33 !    49 1    65 A    81 Q    97  a    113 q
+    2  STX    18 DC2    34 "    50 2    66 B    82 R    98  b    114 r
+    3  ETX    19 DC3    35 #    51 3    67 C    83 S    99  c    115 s
+    4  EOT    20 DC4    36 $    52 4    68 D    84 T    100 d    116 t
+    5  ENQ    21 NAK    37 %    53 5    69 E    85 U    101 e    117 u
+    6  ACK    22 SYN    38 &    54 6    70 F    86 V    102 f    118 v
+    7  BEL    23 ETB    39 '    55 7    71 G    87 W    103 g    119 w
+    8  BS     24 CAN    40 (    56 8    72 H    88 X    104 h    120 x
+    9  HT     25 EM     41 )    57 9    73 I    89 Y    105 i    121 y
+    10 LF     26 SUB    42 *    58 :    74 J    90 Z    106 j    122 z
+    11 VT     27 ESC    43 +    59 ;    75 K    91 [    107 k    123 {
+    12 FF     28 FS     44 ,    60 <    76 L    92 \    108 l    124 |
+    13 CR     29 GS     45 -    61 =    77 M    93 ]    109 m    125 }
+    14 SO     30 RS     46 .    62 >    78 N    94 ^    110 n    126 ~
+    15 SI     31 US     47 /    63 ?    79 O    95 _    111 o    127 DEL
+    */
+}
+
+function strPrintableDcToChar(dc, strCharacterEncoding) {
+    switch (strCharacterEncoding) {
+        case 'ASCII-safe-subset':
+        case 'UTF-8':
+            return implStrFromUnicodeHex(strDcDataLookupByValue("mappings/from/unicode", 1, dc, 0));
+            break;
+        default:
+            eiteError('Unimplemented character encoding: '+strCharacterEncoding);
+            break;
+    }
+}
+
+function dcarrParseDocument(strFormat, bytearrayContent) {
+    switch (strFormat) {
+        case 'sems':
+            return dcarrParseSems(bytearrayContent);
+            break;
+        default:
+            eiteError('Unimplemented document parsing format: '+strFormat);
+            break;
+    }
+}
+
+function dcarrParseSems(bytearrayContent) {
+    // Accepts an array of bytes of a SEMS format document. Returns an array of Dcs.
+    var dcarrParseResults = [];
+    var strParserState = 'dc';
+    var strCurrentDc = '';
+    for (let intByteOffset = 0; intByteOffset < bytearrayContent.byteLength; intByteOffset++) {
+        // do something with each byte in the array. bytearrayContent[intByteOffset] holds the decimal value of the given byte.
+        switch (strParserState) {
+            case 'dc':
+                if (boolIsDigit(bytearrayContent[intByteOffset])) {
+                    strCurrentDc = strCurrentDc + String.fromCharCode(bytearrayContent[intByteOffset]);
+                }
+                if (boolIsSpace(bytearrayContent[intByteOffset])) {
+                    dcarrParseResults.push(strCurrentDc);
+                    strCurrentDc = '';
+                }
+                if (bytearrayContent[intByteOffset] == 35) { // pound sign: start comment
+                    strParserState = 'comment';
+                }
+                break;
+            case 'comment':
+                if (boolIsNewline(bytearrayContent[intByteOffset])) {
+                    strParserState = 'dc';
+                }
+                break;
+        }
+    }
+    return dcarrParseResults;
+}
+
+function dcarrConvertDocument(dcarrInput, strTargetFormat, renderTraits) {
+    dcarrOutput=[]
+    // Build render output buffer for specified format
+    switch (strTargetFormat) {
+        case 'integerList':
+            for (let intInputIndex = 0; intInputIndex < dcarrInput.length; intInputIndex++) {
+                dcarrOutput[intInputIndex] = dcarrInput[intInputIndex];
+            }
+            break;
+        case 'immutableCharacterCells':
+            let intLine=0;
+            dcarrOutput[0] = '';
+            for (let intInputIndex = 0; intInputIndex < dcarrInput.length; intInputIndex++) {
+                if (boolDcIsNewline(dcarrInput[intInputIndex])) {
+                    intLine = intLine + 1;
+                    dcarrOutput[intLine] = '';
+                }
+                if (boolDcIsPrintable(dcarrInput[intInputIndex])) {
+                    dcarrOutput[intLine] = dcarrOutput[intLine] + strPrintableDcToChar(dcarrInput[intInputIndex], renderTraits.characterEncoding);
+                }
+            }
+            break;
+        default:
+            eiteError('Unimplemented document render target format: '+strTargetFormat);
+            break;
+    }
+    return dcarrOutput;
+}
+
+function runDocument(dcarrContent) {
+    strTargetFormat = implGetEnvironmentBestFormat();
+    implDoRenderIo(dcarrConvertDocument(dcarrContent, strTargetFormat, implGetEnvironmentRenderTraits(strTargetFormat)), strTargetFormat);
+}
+// @license-end
